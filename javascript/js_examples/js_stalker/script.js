148e7f47c878de7df6e9827e33e99448b1b850e3
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-03 15:05:32 -0700

    Javascript

---
+++ javascript/js_examples/js_stalker/script.js
@@ -0,0 +1,5 @@
+var name = prompt("What is your name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+alert("Hello " + name + " " + lastName);
+console.log("I heard you were " + age + " years old");
