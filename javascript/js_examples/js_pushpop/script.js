80ea346094d1665b8e99123fa309a4dc7a3c40e3
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-16 13:45:30 -0700

    javascript

---
+++ javascript/js_examples/js_pushpop/script.js
@@ -0,0 +1,9 @@
+//PUSH && POP
+
+var colors = ["red", "orange", "yellow"];
+//push adds an element in an array as last item;
+colors.push("green");
+colors.push("violet");
+//pop removes the last element in an array;
+// colors.pop();
+console.log(colors);
