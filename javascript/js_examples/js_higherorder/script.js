c301987fccec8a9572ab6fa54f1e9ec8c7d6aa19
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-15 23:28:47 -0700

    javascript

---
+++ javascript/js_examples/js_higherorder/script.js
@@ -0,0 +1,11 @@
+// function sing(){
+// 	console.log("twinkle twinkle...");
+// 	console.log("how i wonder...");
+// }
+
+// setInterval(sing, 3000);
+
+setInterval(function(){
+	console.log("This is an anonymous function");
+	console.log("This is OK!");
+}, 2000);
