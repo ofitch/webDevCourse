49a86af2ba0cfb6c7943db459bd82b6dbe9cdf1b
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-16 13:52:07 -0700

    javascript

---
+++ javascript/js_examples/js_shiftunshift/script.js
@@ -0,0 +1,11 @@
+//SHIFT && UNSHIFT
+
+//UNSHIFT to add an element at the beginning of the array
+var colors = ["red", "orange", "yellow"];
+//add as first element in an array, "infrared";
+colors.unshift("infrared");
+
+//Removes first element in an array "infrared";
+colors.shift();
+//removes "red";
+colors.shift();
