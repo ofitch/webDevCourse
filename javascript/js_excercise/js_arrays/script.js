e51347ada39496af9b21d78292ad2f3cad88379d
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-16 14:32:12 -0700

    javacript

---
+++ javascript/js_excercise/js_arrays/script.js
@@ -0,0 +1,16 @@
+//excercise 1
+var numbers = [22, 67, 33, 96, 88];
+//what does the following line print?
+console.log(numbers[numbers.length]);
+
+// to print last number
+console.log(numbers[numbers.length - 1]);
+
+//Excercise 2
+var friendGroups =[
+["Harry", "Ron", "Hermione"],
+["Malfoy", "Crabbe", "Goyle"],
+["Mooney", "Wormtail", "Prongs"]
+];
+//what is the result of this line;
+console.log(friendGroups[2][0]);
