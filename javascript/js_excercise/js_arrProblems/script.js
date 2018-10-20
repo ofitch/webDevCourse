9df3ab83bf5511ad7c9aa16377358c1e87f46481
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-20 12:06:28 -0700

    javascript unfinished problems

---
+++ javascript/js_excercise/js_arrProblems/script.js
@@ -0,0 +1,39 @@
+//Print reverse	input: printReverse([1,2,3,4]);
+//4
+//3
+//2
+//1
+function	prntReverse(input){
+	for(var i = input.length -1; i >= 0; i--){
+		console.log(input[i]);
+	}
+}
+
+//Write a function isUniform() which takes an array as an
+//arguement and returns true if all elements in the array are identical
+function	isUniform(arr){
+	var result;
+	for(var i = 0; i < arr.length; i++){
+		if(arr[0] === arr[i])
+			result = true;
+		else if(arr[0] !== arr[i]){
+			return false;	
+		}
+	}
+	return result;
+}
+
+//Write function sumArray(); accepts an array of num. and
+//returns the sum of all numbers in the array
+function	sumArray(arr){
+	var result = 0;
+	for(var i = 0; i < arr.length; i++){
+		if((typeof(arr[i]) !== "number"))
+			return NaN;
+		else
+			result += arr[i];
+	}
+	return result;
+}
+
+//Not Complete WATCH VIDEO
