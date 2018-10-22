3d4285a235390edf8930c6885212c55c900a1ea5
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-22 14:23:47 -0700

    javascript

--- javascript/js_excercise/js_arrProblems/script.js
+++ javascript/js_excercise/js_arrProblems/script.js
@@ -36,4 +36,14 @@ function	sumArray(arr){
 	return result;
 }
 
-//Not Complete WATCH VIDEO
+//Write a function max() that accepts an array of numbers
+//and returns the maximum number in the array
+
+function	max(arr){
+	var max = arr[0];
+	for(var i = 1; i < arr.length; i++){
+		if(arr[i] > max)
+			max = arr[i];
+	}
+	return max;
+}
