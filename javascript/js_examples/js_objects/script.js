e69ad9bb714a4d7fd72ef0c40d0701678d5e4449
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-22 20:08:11 -0700

    javascript

---
+++ javascript/js_examples/js_objects/script.js
@@ -0,0 +1,28 @@
+//Two ways of writing an object for now;
+var methodOne = {
+	name: "Oscar",
+	last: "fitch",
+	age: 25
+};
+
+var methodTwo = {}
+methodTwo.first = "Emmanuel";
+methodTwo.last = "Fierro";
+methodTwo.age = 25;
+
+//Arrays and objects
+
+var posts = [
+	{
+		title: "Cats are beauty",
+		author: "Oscar",
+		comments: ["Awesome post, ", "You are right!"]
+
+	},
+	{
+		title: "Cats are annoying man!",
+		author: "Anonymous",
+		comments: ["Die!", "Wow, racist"]
+	}
+
+];
