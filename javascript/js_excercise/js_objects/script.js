76b2cc128675089b0345f8a90e64be134ffe020e
Oscar Fitch <osiv.fitch@gmail.com>
2018-10-24 00:15:26 -0700

    javascript

---
+++ javascript/js_excercise/js_objects/script.js
@@ -0,0 +1,28 @@
+//Excercise #1
+
+var someObject = {};
+
+someObject.name = "Oscar",
+someObject.age = 25;
+
+var prop = "color";
+
+someObject[prop] = "red",
+
+someObject[123] = true;
+
+
+//Excercise #2
+
+var object = {
+	friends: [
+		{name: "Doug"},
+		{name: "Patty"},
+		{name: "Tito"}
+	],
+	color: [
+		{name: "green"},
+		{name: "yellow"},
+		{name: "blue"}
+	]
+};
